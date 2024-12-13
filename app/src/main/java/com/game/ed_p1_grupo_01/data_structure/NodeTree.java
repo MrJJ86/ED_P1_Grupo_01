95cf4aa48ce18ccac18a6216ab872fedf1f04626
Joseph <carrerasje@gmail.com>
2024-12-13 14:22:28 -0500

    Agregando la estructura de árbol

---
+++ app/src/main/java/com/game/ed_p1_grupo_01/data_structure/NodeTree.java
@@ -0,0 +1,37 @@
+package com.game.ed_p1_grupo_01.data_structure;
+
+import java.util.LinkedList;
+
+public class NodeTree<E> {
+    private E content;
+    private LinkedList<Tree<E>> children;
+
+    public NodeTree(E content){
+        this.content = content;
+        this.children = new LinkedList<>();
+    }
+
+    public E getContent() {
+        return content;
+    }
+
+    public void setContent(E content) {
+        this.content = content;
+    }
+
+    public LinkedList<Tree<E>> getChildren() {
+        return children;
+    }
+
+    public void setChildren(LinkedList<Tree<E>> children) {
+        this.children = children;
+    }
+
+    public Tree<E> getChild(int index) {
+        return this.children.get(index);
+    }
+
+    public boolean setChild(Tree<E> tree) {
+        return this.children.add(tree);
+    }
+}
---
+++ app/src/main/java/com/game/ed_p1_grupo_01/data_structure/Tree.java
@@ -0,0 +1,34 @@
+package com.game.ed_p1_grupo_01.data_structure;
+
+public class Tree<E> {
+    private NodeTree<E> root;
+
+    public Tree(){
+        this.root = null;
+    }
+    public Tree(NodeTree<E> root){
+        this.root = root;
+    }
+    public Tree(E content){
+        this.root = new NodeTree<>(content);
+    }
+
+    public NodeTree<E> getRoot() {
+        return root;
+    }
+
+    public void setRoot(NodeTree<E> root) {
+        this.root = root;
+    }
+
+    public boolean isEmpty(){
+        return this.root == null;
+    }
+
+    public boolean isLeaf(){
+        if(!this.isEmpty()){
+            return this.root.getChildren().isEmpty();
+        }
+        return false;
+    }
+}
