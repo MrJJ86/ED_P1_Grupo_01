5db8194c8ce38d0407590ca64262f756491fca7e
And0pi <151966785+And0pi@users.noreply.github.com>
2024-12-14 00:52:46 -0500

    Creacion de la funcion p 1.0

--- app/src/main/java/com/game/ed_p1_grupo_01/MainActivity.java
+++ app/src/main/java/com/game/ed_p1_grupo_01/MainActivity.java
@@ -1,7 +1,8 @@
 package com.game.ed_p1_grupo_01;
 
 import android.os.Bundle;
-
+import android.widget.GridLayout;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -21,4 +22,38 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    private boolean lineajugable(GridLayout table, int[] comb,String ficha){
+
+        for(int i=0;i<comb.length;i++){
+            Button boton= (Button) table.getChildAt(comb[i]);
+            String cont=boton.getText().toString();
+
+            if(!cont.equals(ficha) && !cont.isEmpty()){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int p(GridLayout table,String ficha) {
+        int p = 0;
+
+        int[][] combinaciones = {
+                {0, 1, 2}, {3, 4, 5}, {6, 7, 8},  //Lineas horizontales
+                {0, 4, 8}, {2, 4, 6},  //Lineas diagonales
+                {0, 3, 6}, {1, 4, 7}, {2, 5, 8}  //Lineas Verticales
+        };
+
+        for (int i = 0; i < combinaciones.length; i++) {
+            int[] comb = combinaciones[i];
+            if (lineajugable(table, comb, ficha)) {
+                p++;
+            }
+        }
+
+        return p;
+    }
+
+
 }
