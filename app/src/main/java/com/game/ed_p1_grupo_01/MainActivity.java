242783606bd7148e2e9a86d4e7b9d1550d20665f
And0pi <151966785+And0pi@users.noreply.github.com>
2024-12-23 00:26:38 -0500

    Creacion de la funcion p 2.0

--- app/src/main/java/com/game/ed_p1_grupo_01/MainActivity.java
+++ app/src/main/java/com/game/ed_p1_grupo_01/MainActivity.java
@@ -1,13 +1,13 @@
 package com.game.ed_p1_grupo_01;
 
 import android.os.Bundle;
-import android.widget.GridLayout;
-import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+//import com.game.ed_p1_grupo_01.elements.Token;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,37 +23,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private boolean lineajugable(GridLayout table, int[] comb,String ficha){
-
-        for(int i=0;i<comb.length;i++){
-            Button boton= (Button) table.getChildAt(comb[i]);
-            String cont=boton.getText().toString();
-
-            if(!cont.equals(ficha) && !cont.isEmpty()){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public int p(GridLayout table,String ficha) {
-        int p = 0;
-
-        int[][] combinaciones = {
-                {0, 1, 2}, {3, 4, 5}, {6, 7, 8},  //Lineas horizontales
-                {0, 4, 8}, {2, 4, 6},  //Lineas diagonales
-                {0, 3, 6}, {1, 4, 7}, {2, 5, 8}  //Lineas Verticales
-        };
-
-        for (int i = 0; i < combinaciones.length; i++) {
-            int[] comb = combinaciones[i];
-            if (lineajugable(table, comb, ficha)) {
-                p++;
-            }
-        }
-
-        return p;
-    }
 
 
 }
--- app/src/main/java/com/game/ed_p1_grupo_01/modelo/operations/TableOperation.java
+++ app/src/main/java/com/game/ed_p1_grupo_01/modelo/operations/TableOperation.java
@@ -1,5 +1,10 @@
 package com.game.ed_p1_grupo_01.modelo.operations;
 
+import android.widget.Button;
+import android.widget.GridLayout;
+
+import com.game.ed_p1_grupo_01.modelo.Token;
+
 public class TableOperation {
     public static final int[][][] POSITIONS ={
             //Horizontal
@@ -7,11 +12,43 @@ public class TableOperation {
             {{1,0},{1,1},{1,2}},
             {{2,0},{2,1},{2,2}},
             //Vertical
-            {{0,1},{1,0},{2,0}},
+            {{0,0},{1,0},{2,0}},
             {{0,1},{1,1},{2,1}},
             {{0,2},{1,2},{2,2}},
             //Diagonal
             {{0,0},{1,1},{2,2}},
             {{0,2},{1,1},{2,0}}
     };
+
+    private boolean lineajugable(GridLayout table,int[][] comb, Token ficha){
+
+        for(int i=0;i< comb.length;i++){
+            int fila = comb[i][0];
+            int columna = comb[i][1];
+            int linealIndex = fila * 3 + columna;
+            Button boton= (Button) table.getChildAt(linealIndex);
+            Token tok=(Token) boton.getTag();
+
+            if(!(tok==null) && !tok.equals(ficha) ){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int p(GridLayout table, Token ficha) {
+        int p = 0;
+
+        for (int i = 0; i < POSITIONS.length; i++) {
+                int[][] comb = POSITIONS[i];
+                if (lineajugable(table,comb, ficha)) {
+                    p++;
+                }
+        }
+
+        return p;
+    }
+
+
+
 }
